34955bbb7d947f6bc68819a8db9bfe372e87a1b0
Derrick Ampire <ampire90@gmail.com>
2023-06-09 15:14:53 +0300

    cache

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Forum
 
 
 def home(request):
-    return HttpResponse('Hello World!')
+    forums = Forum.objects.all()
+    return render(request, 'home.html', {'forums': forums})
