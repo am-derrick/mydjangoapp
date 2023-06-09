bdda8289315e3ab6c63f78ccb5b6f70dc8d1201f
Derrick Ampire <ampire90@gmail.com>
2023-06-09 14:05:42 +0300

    __str__ representation of forum class

--- forum/models.py
+++ forum/models.py
@@ -7,6 +7,9 @@ class Forum(models.Model):
     name = models.CharField(max_length=24, unique=True)
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     """class representing topic under a forum"""
