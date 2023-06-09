0b3771bdf377475a72269ad32d21d828d3aacabd
Derrick Ampire <ampire90@gmail.com>
2023-06-09 15:31:22 +0300

    test cases added

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import home
 
-# Create your tests here.
+
+class HomepageTests(TestCase):
+    def test_home_view_status_code(self):
+        """tests response code for home page"""
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        """resolves / url to the home view"""
+        view = resolve('/')
+        self.assertEquals(view.func, home)
