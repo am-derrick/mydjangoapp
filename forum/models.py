3f13ede4a80a5c57c7758f5fb78a1007c68e538f
Derrick Ampire <ampire90@gmail.com>
2023-06-09 10:29:12 +0300

    class models created

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Forum(models.Model):
+    """Forum/Board class representing the name and description"""
+    name = models.CharField(max_length=24, unique=True)
+    description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    """class representing topic under a forum"""
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    forum = models.ForeignKey(
+        Forum, related_name='topics', on_delete=models.CASCADE)
+    opener = models.ForeignKey(
+        User, related_name='topics', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    """class representing a post on the forum"""
+    message = models.TextField(max_length=400)
+    topic = models.ForeignKey(
+        Topic, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(
+        User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(
+        User, null=True, related_name='+', on_delete=models.CASCADE)
