20694c1162c9be9d9fa3625d6a29611a868a900b
Derrick Ampire <ampire90@gmail.com>
2023-06-12 14:43:04 +0300

    more test cases added to home page

--- forum/tests.py
+++ forum/tests.py
@@ -5,16 +5,28 @@ from .models import Forum
 
 
 class HomepageTests(TestCase):
+    def setUp(self):
+        """creates a forum instance"""
+        Forum.objects.create(
+            name="Banter", description="This forum is about random banter.")
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         """tests response code for home page"""
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         """checks to resolve / url to the home view"""
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        """tests navigation links to topics page"""
+        forum_topics_url = reverse(
+            'forum_topics', kwargs={'pk': self.forum.pk})
+        self.assertContains(
+            self.response, 'href="{0}"'.format(forum_topics_url))
 
 
 class ForumTopicsTests(TestCase):
