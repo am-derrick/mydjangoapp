df721c62d350c11fd7b52f27788058241256d7a9
Derrick Ampire <ampire90@gmail.com>
2023-06-12 13:47:15 +0300

    forum topics view added

--- forum/views.py
+++ forum/views.py
@@ -4,5 +4,12 @@ from .models import Forum
 
 
 def home(request):
+    """renders home page view"""
     forums = Forum.objects.all()
     return render(request, 'home.html', {'forums': forums})
+
+
+def forum_topics(request, pk):
+    """renders page containing forums"""
+    forum = Forum.objects.get(pk=pk)
+    return render(request, 'topics.html', {'forum': forum})
