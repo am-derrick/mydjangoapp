07be30df100781f33870bc58f279816b0cff657e
Derrick Ampire <ampire90@gmail.com>
2023-06-13 13:29:13 +0300

    data retrieval methods added

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Forum
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Forum, Topic, Post
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -17,4 +18,26 @@ def forum_topics(request, pk):
 def new_topic(request, pk):
     """renders page where a user can add a new topic"""
     forum = get_object_or_404(Forum, pk=pk)
+
+    if request.method == 'POST':
+        theme = request.POST['theme']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: implement later, get user logged in
+
+        topic = Topic.objects.create(
+            subject=theme,
+            forum=forum,
+            opener=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        # TODO: redirect user to created topic page
+        return redirect('forum_topics', pk=forum.pk)
+
     return render(request, 'new_topic.html', {'forum': forum})
