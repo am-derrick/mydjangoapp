3dde5040cb464ae089db1431fbda86d2312c20ea
Derrick Ampire <ampire90@gmail.com>
2023-06-13 12:33:02 +0300

    test cases added

--- forum/tests.py
+++ forum/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home, forum_topics
+from .views import home, forum_topics, new_topic
 from .models import Forum
 
 
@@ -43,13 +43,13 @@ class ForumTopicsTests(TestCase):
             name="Banter", description="This forum is about random banter.")
 
     def test_forum_topics_view_success_status_code(self):
-        """test case for success of status code"""
+        """test case for success of forum_topics page"""
         url = reverse('forum_topics', kwargs={'pk': 1})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
     def test_forum_topics_view_not_found_status_code(self):
-        """test case for when not found status code"""
+        """test case for when not found forum_topics page"""
         url = reverse('forum_topics', kwargs={'pk': 98})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
@@ -58,3 +58,34 @@ class ForumTopicsTests(TestCase):
         """tests forum_topics url"""
         view = resolve('/forum/1/')
         self.assertEquals(view.func, forum_topics)
+
+
+class NewTopicTests(TestCase):
+    def setUp(self):
+        """creates a new topics instance"""
+        Forum.objects.create(
+            name='Banter', description='This forum is about random banter.')
+
+    def test_new_topic_view_success_status_code(self):
+        """test case for success of new_topics page"""
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_view_not_found_status_code(self):
+        """test case for when new_topics page is not found"""
+        url = reverse('new_topic', kwargs={'pk': 98})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_new_topic_url_resolves_new_topic_view(self):
+        """tests new_topics url resolve"""
+        view = resolve('/forum/1/new/')
+        self.assertEquals(view.func, new_topic)
+
+    def test_new_topic_view_links_to_forum_topics_view(self):
+        """tests that new_topic has link that links back to forum_topics"""
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        forum_topics_url = reverse('forum_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, 'href="{0}"'.format(forum_topics_url))
