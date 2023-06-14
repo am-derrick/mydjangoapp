42fffb4f65530337b0f9340d7355ffc2d60c9711
Derrick Ampire <ampire90@gmail.com>
2023-06-14 12:06:00 +0300

    new POST method to include forms API

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Forum, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 
 def home(request):
@@ -18,26 +19,22 @@ def forum_topics(request, pk):
 def new_topic(request, pk):
     """renders page where a user can add a new topic"""
     forum = get_object_or_404(Forum, pk=pk)
+    user = User.objects.first()  # TODO: implement, get user logged in
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: implement later, get user logged in
-
-        topic = Topic.objects.create(
-            subject=subject,
-            forum=forum,
-            opener=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        # TODO: redirect user to created topic page
-        return redirect('forum_topics', pk=forum.pk)
-
-    return render(request, 'new_topic.html', {'forum': forum})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.forum = forum
+            topic.opener = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            # TODO: redirect to the created topic page
+            return redirect('forum_topics', pk=forum.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'forum': forum, 'form': form})
