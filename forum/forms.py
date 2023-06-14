8f088d958a7339beff11fab4dfaed66e2042f28e
Derrick Ampire <ampire90@gmail.com>
2023-06-14 12:18:35 +0300

    attribute added to text area

--- forum/forms.py
+++ forum/forms.py
@@ -3,8 +3,11 @@ from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(
-    ), max_length=4000, help_text='Maximum text length is 4000.')
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': 'Start a conversation/thread.'}),
+        max_length=4000,
+        help_text='Maximum text length is 4000.')
 
     class Meta:
         model = Topic
