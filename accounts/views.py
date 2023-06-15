d14d285a48c51efaf799a00635a5543e340f56ac
Derrick Ampire <ampire90@gmail.com>
2023-06-15 10:52:30 +0300

    post method added

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as user_login
 
 
 def signup(request):
     """sign up view"""
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
