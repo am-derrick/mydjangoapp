e7ea27d74cdc6125cbb784553a3e73d99095c45d
Derrick Ampire <ampire90@gmail.com>
2023-06-15 10:27:02 +0300

    tests for sign up view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from .views import signup
 
-# Create your tests here.
+
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        """tests that the sign up page returns success status"""
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_resolves_signup_view(self):
+        """tests that the sign up view / resolves"""
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
