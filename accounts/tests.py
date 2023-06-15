e8777db0deac78a3b79f0e15c92ddba21b579408
Derrick Ampire <ampire90@gmail.com>
2023-06-15 12:22:34 +0300

    test cases for successful sign up

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from .views import signup
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class SignUpTests(TestCase):
@@ -27,3 +28,33 @@ class SignUpTests(TestCase):
         """tests that sign up page contains form"""
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessSignUpTests(TestCase):
+    def setUp(self):
+        """initialises tests for successful sign up"""
+        url = reverse('signup')
+        data = {
+            'username': 'new_user',
+            'password1': 'new_password',
+            'password2': 'new_password'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirected(self):
+        """
+        tests that valid user is redirected
+        when form is valid, user is directed to home page
+        """
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_created(self):
+        """tests whether user has been created"""
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authenticated(self):
+        """tests that user is successfully authenticated"""
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
