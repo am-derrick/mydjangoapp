b8996f0f67ba849c6b02b744a1406f3faf622cad
Derrick Ampire <ampire90@gmail.com>
2023-06-20 22:27:13 +0300

    more test cases added

--- accounts/tests/test_password_reset_view.py
+++ accounts/tests/test_password_reset_view.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
-from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView,
+                                       PasswordResetConfirmView)
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
 from django.urls import resolve, reverse
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 
 class PasswordResetTests(TestCase):
@@ -66,3 +70,72 @@ class PasswordResetDoneTests(TestCase):
         """tests password reset done view func"""
         view = resolve('/reset/done/')
         self.assertEquals(view.func.view_class, PasswordResetDoneView)
+
+
+class PasswordResetConfirmTests(TestCase):
+    def setUp(self):
+        """
+        initialises tests for password reset confirmation
+
+        Create a valid password reset token 
+        https://github.com/django/django/blob/1.11.5/django/contrib/auth/forms.py -- line 280 & 282
+        """
+        user = User.objects.create_user(
+            username='john', email='john@doe.com', password='123abc')
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        self.token = default_token_generator.make_token(user)
+        url = reverse('password_reset_confirm', kwargs={
+                      'uidb64': self.uid, 'token': self.token})
+        self.response = self.client.get(url, follow=True)
+
+    def test_status_code(self):
+        """tests success of view"""
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        """tests view function for password reset confirm"""
+        view = resolve(
+            '/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
+        self.assertEquals(view.func.view_class, PasswordResetConfirmView)
+
+    def test_contains_csrf(self):
+        """tests that view has csrf token"""
+        self.assertContains(self.response, 'csrfmiddletoken')
+
+    def test_contains_form(self):
+        """tests that password rest confirm view has set password form"""
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SetPasswordForm)
+
+    def test_form_inputs(self):
+        """tests the inputs (csrf and two password fields) in the password reset confirm form"""
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="password"', 2)
+
+
+class InvalidPasswordResetConfirmTests(TestCase):
+    def setUp(self):
+        """initialises invalid password reset confirm tests"""
+        user = User.objects.create_user(
+            username="john", email="john@doe.com", password="123abc")
+        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        token = default_token_generator.make_token(user)
+        user.set_password('456xyz')  # change password to invalidate token
+        user.save
+        url = reverse('password_reset_confirm', kwargs={
+                      'uidb64': uid, 'token': token})
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        """
+        tests status code returns 200 when invalid password reset confirm
+        invalid reset still returns a 200 status code
+        """
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_html(self):
+        """tests that when invalid, link to password reset is present"""
+        password_reset_url = reverse('password_reset')
+        self.assertContains(self.response, 'Password reset link invalid')
+        self.assertContains(
+            self.response, 'href="{0}"'.format(password_reset_url))
